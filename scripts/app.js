54325ddec79e5a453705fcc0447112965da6a86e
Eugenio Mendoza <eu.mendoza@gmail.com>
2015-11-12 12:57:03 +0100

    Added City Search

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,26 @@
 (function(document){
 	'use strict';
+
 	var app = document.querySelector('#app');
 
-	
+	window.addEventListener('WebComponentsReady', function() {
+		
+		// imports are loaded and elements have been registered
+
+
+		app.openDialog = function(event){
+			var id = event.currentTarget.getAttribute('data-dialog');
+			var dialog = document.getElementById(id);
+			if (dialog){
+				dialog.open();
+			}
+		};
+
+		app.setCurrentCity = function(){
+			var city = document.getElementById('input-currentcity').value;
+			app.currentcity = city;
+		};
+
+	});
 
 })(document);
